7b8e6c8375538b04efc247b5176443a375b22779
oviola01 <oviola01@gmail.com>
2023-02-23 19:04:30 +0100

    Equalizer

--- metodusgyak.java
+++ metodusgyak.java
@@ -10,12 +10,7 @@ package com.mycompany.method;
  */
 public class Metodusgyak {
     public static void main(String[] args){
-        String kimenet = "Az első 10 szám összege: " + elso10SzamOsszege();
-        kiir(kimenet);
-        
-        int szam1 = 8, szam2 = 3, szam3 = 2, szam4 = 11;
-        kiir("%d + %d = %d\n".formatted(szam1, szam2, osszead(szam1, szam2)));
-        kiir("%d + %d + %d+ %d = %d\n".formatted(szam1, szam2, szam3, szam4, osszead(osszead(szam1, szam2),osszead(szam3, szam4))));
+        feladatok();
     }   
     
     
@@ -36,4 +31,30 @@ public class Metodusgyak {
     private static void kiir(String szoveg) {
         System.out.print(szoveg);
     }
+    
+    private static void feladat_10szam() {
+        String kimenet = "Az első 10 szám összege: " + elso10SzamOsszege();
+        kiir(kimenet);
+    }
+    
+    private static void feladat_sum2szam(int szam1, szam2) {
+        kiir("%d + %d = %d\n".formatted(szam1, szam2, osszead(szam1, szam2)));
+    }
+
+    private static void feladat_sum4szam(int szam1, szam2, szam3 szam4) {
+        kiir("%d + %d + %d+ %d = %d\n".formatted(szam1, szam2, szam3, szam4, osszead(osszead(szam1, szam2),osszead(szam3, szam4))));
+    }
+    
+    private static void feladat_gyok3szam(int szam1, szam2, szam3) {
+        int osszegem = osszead(osszead(szam1, szam2), szam3);
+        kiir(szam1"+"szam2"+"szam3"gyöke: "Math.sqrt(osszegem));
+    }
+
+    private static void feladatok() {
+        feladat_10szam();
+        feladat_sum2szam(8, 3);
+        feladat_sumszam(5, 8, 2, 11);
+        feladat_gyok3szam(3, 8, 2);
+    }
+    
 }
